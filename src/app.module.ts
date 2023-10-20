85e4bc9ab9ceae8b167366b288eb3b878c5e415f
Victor Nogueira <victornogu80@gmail.com>
2023-10-20 11:33:15 -0300

    Criando módulo de requerentes (applicants)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 // import { MongooseModule } from '@nestjs/mongoose';
+import { ApplicantsModule } from './applicants/applicants.module';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
+    ApplicantsModule,
     // MongooseModule.forRoot('mongodb://localhost/nest'),
   ],
 })
