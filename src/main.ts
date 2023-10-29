c5e32627d8c04e77b1fdcd34b656029a56386e51
Victor Nogueira <victornogu80@gmail.com>
2023-10-29 12:39:29 -0300

    Inserindo biblioteca cors

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: '*',
-    methods: '*',
-  });
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5175');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
   const config = new DocumentBuilder()
     .setTitle('API Italian Consulate')
