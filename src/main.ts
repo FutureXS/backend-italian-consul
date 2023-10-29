c975061014e65a8ed04496e10f44607ffa0748ab
Victor Nogueira <victornogu80@gmail.com>
2023-10-29 11:35:23 -0300

    Alteração cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
   });
   app.useGlobalPipes(new ValidationPipe());
 
