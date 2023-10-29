0e564ac7f1ffcc60452fce820310a404a5bb0701
Victor Nogueira <victornogu80@gmail.com>
2023-10-29 12:13:57 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ async function bootstrap() {
     methods: '*',
   });
   app.useGlobalPipes(new ValidationPipe());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5175');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
   const config = new DocumentBuilder()
     .setTitle('API Italian Consulate')
