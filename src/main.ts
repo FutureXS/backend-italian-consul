c555f4b217c3f0b2a96d5f110b019b310d744dd7
Pedro <pedro.bessa@critical-links.com>
2023-11-07 18:56:00 -0300

    Versioning untracked files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import express from 'express';
 
 const fs = require('fs');
 
