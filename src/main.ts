fdbda308267e21c9d86cc8a277681eb8b65f5665
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2023-11-08 13:43:22 -0300

    alterando porta

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
-const fs = require('fs');
-
+const fs = require("fs");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true }); // No explicit type for NestExpressApplication
 
   app.useGlobalPipes(new ValidationPipe());
 
-
-
-
   const config = new DocumentBuilder()
-    .setTitle('API Italian Consulate')
-    .setDescription('API Italian Consulate')
-    .setVersion('1.0')
-    .addTag('consul')
+    .setTitle("API Italian Consulate")
+    .setDescription("API Italian Consulate")
+    .setVersion("1.0")
+    .addTag("consul")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  await app.listen(80);
 }
 
 bootstrap();
